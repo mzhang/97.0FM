29d72789019604f8aca0ae8a24b10798b137bf0b
Matt Zhang <67244273+mzhang@users.noreply.github.com>
2020-11-03 21:19:12 -0500

    search function working

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
+from ytmusicapi import YTMusic
 
+ytmusic = YTMusic()
 client = commands.Bot(command_prefix = ".")
 
 @client.event
@@ -10,6 +12,15 @@ async def on_ready():
 @client.command()
 async def beep(ctx):
     await ctx.send(f'boop {client.latency * 1000}')
+
+@client.command()
+async def search(ctx, *, query):
+    search = ytmusic.search(query, 'songs')[0]['videoId']
+    
+
+    await ctx.send(f'https://www.youtube.com/watch?v={search}')
+
+    
 
 
 client.run('NzczMzM5MzY0NDQ0MjA5MTc0.X6HyaA.whMtIsCYhj-fYDmCeDpHHKewzBU')
