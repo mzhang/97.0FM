a26b4f8ae1dfe9d7137b85949d8e8d501915a8ed
mzhang <zugzhang.com>
2020-11-03 21:39:09 -0500

    gets radioed playlist

--- test.py
+++ test.py
@@ -3,8 +3,12 @@ import json
 
 ytmusic = YTMusic()
 search = ytmusic.search('aaaa', 'songs')
-playlist = ytmusic.create_playlist('a','a')
-ytmusic.add_playlist_items(playlist, [search])
+# playlist = ytmusic.create_playlist('a','a')
+# ytmusic.add_playlist_items(playlist, [search])
+
+playlist = ytmusic.get_watch_playlist(search[0]['videoId'])
+
+
 print(playlist)
 
 
