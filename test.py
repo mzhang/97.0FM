7c6f677dcfa237e32c7aa6d6c50fd594c98177b5
mzhang <zugzhang.com>
2020-11-03 21:48:48 -0500

    fully working

--- test.py
+++ test.py
@@ -7,8 +7,12 @@ search = ytmusic.search('aaaa', 'songs')
 # ytmusic.add_playlist_items(playlist, [search])
 
 playlist = ytmusic.get_watch_playlist(search[0]['videoId'])
+out = 'https://www.youtube.com/watch?v='
+for entry in playlist:
+    for key in entry:
+        if key == 'videoId':
+            out+=(entry['videoId'] + ',')
 
-
-print(playlist)
+print(out)
 
 
