767aee981ff932593e8f90846a45cf8f46a7256b
mzhang <zugzhang.com>
2020-11-03 23:20:49 -0500

    formatted output into embed

--- bot.py
+++ bot.py
@@ -13,20 +13,25 @@ async def on_ready():
 async def beep(ctx):
     await ctx.send(f'boop {client.latency * 1000}')
 
-@client.command()
+@client.command(aliases=['s', 'S'])
 async def search(ctx, *, query):
     search = ytmusic.search(query, 'songs')
     playlist = ytmusic.get_watch_playlist(search[0]['videoId'])
+    
+    embed=discord.Embed(title="Here's a playlist based on your last song:", description="These are the songs that our Google overlords thinks is best for you! ", color=0xae00ff)
+    embed.set_thumbnail(url=search[0]['thumbnails'][2]['url'])
+    
+    
     out = 'http://www.youtube.com/watch_videos?video_ids='
     for entry in playlist:
         for key in entry:
             if key == 'videoId':
                 out+=(entry['videoId'] + ',')
-    out = out[:-1]
-    await ctx.send(out)
+                embed.add_field(name=entry['title'],value="https://www.youtube.com/watch?v="+entry['videoId'])
+    out = '```' + out[:-1] + '```'
 
-    
-
+    embed.add_field(name="Triple-click and copy the following link! ", value=out)
+    await ctx.send(embed=embed)
 
 client.run('NzczMzM5MzY0NDQ0MjA5MTc0.X6HyaA.whMtIsCYhj-fYDmCeDpHHKewzBU')
 
--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 from ytmusicapi import YTMusic
 import json
+import discord
 
 ytmusic = YTMusic()
 search = ytmusic.search('aaaa', 'songs')
-# playlist = ytmusic.create_playlist('a','a')
-# ytmusic.add_playlist_items(playlist, [search])
 
 playlist = ytmusic.get_watch_playlist(search[0]['videoId'])
 out = 'https://www.youtube.com/watch?v='
+
+print(search[0]['thumbnails'][2]['url'])
+
+
 for entry in playlist:
     for key in entry:
         if key == 'videoId':
             out+=(entry['videoId'] + ',')
 
-print(out)
-
-
+print(f"```{out}```")
