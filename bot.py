ebcf69b7d1c23f6773b1d0eb9a721d89207a9052
Matt Zhang <mcmattzhang@gmail.com>
2021-01-15 18:44:27 -0500

    .play now plays song

--- bot.py
+++ bot.py
@@ -87,4 +87,42 @@ async def fullSearch(ctx, *, query):
     except Exception as e:
         print(e)
         await ctx.send('A search for'+ """ `"""+query+"""` """ + 'yielded no results! Maybe try rephrasing the query?')
+
+from youtube_dl import YoutubeDL
+from requests import get
+
+#Get videos from links or from youtube search
+def search(query):
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+        try: requests.get(query)
+        except: info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+        else: info = ydl.extract_info(query, download=False)
+    return (info, info['formats'][0]['url'])
+
+async def join(ctx, voice):
+    channel = ctx.author.voice.channel
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect() 
+        return voice
+
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+from discord.utils import get
+
+@client.command()
+async def play(ctx, *, query):
+    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+    video, source = search(query)
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    voice = await join(ctx, voice)
+    await ctx.send(f'Now live!')
+
+    voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+    voice.is_playing()
+
 client.run(token)
